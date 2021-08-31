957d16afd33c298f94ee06900261943cfa8ab612
wuxiaohong <wuxiaohong@tcl.com>
2021-08-31 20:34:26 +0800

    init

---
+++ entry/src/main/java/com/example/testapplication/MyApplication.java
@@ -0,0 +1,10 @@
+package com.example.testapplication;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
